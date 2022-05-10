ff953233437956fa94f5ed77f9dc3f90ffb72a17
teamit11 <103455230+teamit11@users.noreply.github.com>
2022-05-10 14:55:22 +0200

    Update java.js

--- java/java.js
+++ java/java.js
@@ -8,3 +8,20 @@ dark.addEventListener("click", (eo) => {
 body.classList.toggle("dark")
 
 })
+
+// Menu Bar show and hide //
+const navbar =document.getElementById("nav");
+
+
+document.getElementById("show").onclick = function() {showmenu()};
+
+function showmenu() {
+    navbar.style.right = "0";
+}
+
+
+document.getElementById("hide").onclick = function() {hidemenu()};
+
+function hidemenu() {
+    navbar.style.right = "-900px";
+}
